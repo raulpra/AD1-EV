b965fa45353255fb743002e54659b6f389cd4b0d
raulpra <raulprada@gmail.com>
2025-12-11 21:42:10 +0100

    creación proyecto Spring Boot

---
+++ API-Inmobiliaria/src/main/java/org/inmobiliaria/apiinmobiliaria/ApiInmobiliariaApplication.java
@@ -0,0 +1,13 @@
+package org.inmobiliaria.apiinmobiliaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInmobiliariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInmobiliariaApplication.class, args);
+	}
+
+}
